71a5c4e0fdd4c3850214c248dfdd947400b29d05
umayer10-AI <mdumayer10@gmail.com>
2026-05-24 19:54:50 +0600

    add

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,12 @@
+import Banner1 from '@/component/Banner1';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
-      Hello
+      <div>
+        <Banner1></Banner1>
+      </div>
     </div>
   );
 };
